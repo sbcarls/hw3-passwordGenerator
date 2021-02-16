15c7e09cbbdefa655f14b268e5e777f6aed6cec6
Sarah Carlson <sbcarls@gmail.com>
2021-02-16 11:21:28 -0500

    corrected code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,18 +12,19 @@ function writePassword() {
 }
 
 function generatePassword(){
-  const newPassword = "";
-  const options = [];
-  const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let newPassword = "";
+  let options = [];
+  let upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const special = ["!", "@", "#", "$", "%", "^", "&", "*", "?",];
-  const pwLength = ["0", "1"];
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  //const pwLength = ["0", "1"];
 
   const length = prompt("How many characters do you want in your password?");
-  if(length) options = options.concat(pwLength);
+  //if(length) options = options.concat(pwLength);
   //console.log(numChars);
 
-  const specialChars = confirm("DO you want special characters in your password?");
+  const specialChars = confirm("Do you want special characters in your password?");
   if(specialChars) options = options.concat(special);
   //console.log(specialChars);
 
@@ -35,7 +36,10 @@ function generatePassword(){
   if(upperCase) options = options.concat(upperLetters);
   //console.log(upperCase);
 
-  for (let i = 0; i < numChars; i++){
+  const nums = confirm("Do you want numbers in your password?");
+  if(nums) options = options.concat(nums);
+
+  for (let i = 0; i < length; i++){
     const character = Math.floor(Math.random() * options.length);
     newPassword += options[character];
   }
